70dded33dcebd7014a820eef5425e47d62d5746b
Kenneth Thomas <Ken@Kenneths-MacBook-Pro.local>
2017-10-22 16:06:22 -0400

    space after comment

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  #used in seeds file
+  # used in seeds file
   def self.match_primary_key_sequence
     sql = "SELECT pg_catalog.setval(pg_get_serial_sequence('#{table_name}', 'id'), MAX(id)) FROM #{table_name};"
     connection.execute(sql)
