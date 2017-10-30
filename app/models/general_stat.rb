95c1f5d40ff73002bcda65e9c188c9c5ce9ecd79
Kenneth Thomas <Ken@Kenneths-MacBook-Pro.local>
2017-10-30 00:02:19 -0400

    sql

--- app/models/general_stat.rb
+++ app/models/general_stat.rb
@@ -12,12 +12,18 @@ class GeneralStat < ApplicationRecord
 
 private
 
+# inject is used to add together items in an array
   def total_weight
     self.class.previous_stats(@limit, date).inject(0) do |total, stat|
       total + stat.weight.to_f
     end + weight.to_f
   end
 
+  # fetches the last limit items from before the current item's date
+  # generates the following sql:
+    # SELECT  "general_stats"."date", "general_stats"."weight" FROM "general_stats"
+    # WHERE (date < '2017-09-09 00:00:00')
+    # ORDER BY "general_stats"."date" DESC LIMIT $1  [["LIMIT", 6]]
   def self.previous_stats(limit, date = DateTime.now)
     where("date < ?", date).select(:weight).limit(limit).order(date: :desc)
   end
